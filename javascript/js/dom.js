1423ea628a93b23d5351e6bb3ae58935a71dff35
Lazuardi1 <87347424+Lazuardi1@users.noreply.github.com>
2022-01-07 17:23:06 +0700

    Delete javascript directory

--- javascript/js/dom.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("h1").innerText="saya belajar javascript";
-
-document.querySelector("p").innerText="javascript itu mudah";
-
-document.querySelector(".isi").innerText="saya belajar javascript di smk revit";
-
-document.querySelector("#judul").innerText="belajar javascript";
