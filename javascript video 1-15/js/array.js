22a975bcca5e3f558b2428461319cbb1eefc8734
Lazuardi1 <87347424+Lazuardi1@users.noreply.github.com>
2022-01-07 17:24:42 +0700

    Add files via upload

---
+++ javascript video 1-15/js/array.js
@@ -0,0 +1,26 @@
+let coba = function () {
+    return "coba function";
+};
+
+let buah = ["apel",
+"nangka",
+"jeruk",
+10,
+coba(),
+tes = ()=> "hasil retur arrow function",
+function name() {
+    return "lazuardi";
+},
+];
+
+
+console.log(buah);
+console.log(buah[1]);
+
+for (let i in buah) {
+    console.log(buah[1]);
+    
+}
+
+console.log(buah[5]());
+console.log(buah[6]());
---
+++ javascript video 1-15/js/event.js
@@ -0,0 +1,11 @@
+function tampil(r) {
+    a = document.querySelector("p");
+    //a.innertext = "belajar event js";
+    console.log("belajar event js");
+
+}
+
+judul.onclick= function () {
+    console.log("belajar event js menggunakan id");
+    document.querySelector(".isi").innerHTML = "belajar js menggunakan id";
+};
